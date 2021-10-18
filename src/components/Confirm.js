80fa2d968f14f678dadafadd1e434839d11061c6
John Ratana <jratana1@gmail.com>
2021-10-17 23:32:04 -0400

    delete dialog

--- src/components/Confirm.js
+++ src/components/Confirm.js
@@ -11,12 +11,19 @@ import Dialog from '@mui/material/Dialog';
 import PersonIcon from '@mui/icons-material/Person';
 import AddIcon from '@mui/icons-material/Add';
 import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box'
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
 import { blue } from '@mui/material/colors';
+
+import { BASE_URL } from '../App'
+
 
 const emails = ['username@gmail.com', 'user02@gmail.com'];
 
 export default function Confirm(props) {
-  const { onClose, open, action, rowData } = props;
+  const { onClose, open, action, rowData, setRows } = props;
 
   const handleClose = () => {
     onClose();
@@ -24,6 +31,7 @@ export default function Confirm(props) {
 
   const handleListItemClick = (value) => {
     onClose(value);
+
   };
 
   const deletePatient = (id) => {
@@ -32,14 +40,14 @@ export default function Confirm(props) {
         headers: {
             'Content-Type': 'application/json',
             'Accept': 'application/json',
-            'body': {id: id}
         },
     }
 
-      fetch(BASE_URL+"patients", config)
+      fetch(BASE_URL+`patients/${id}`, config)
       .then(res => res.json())
       .then(res => {
           setRows(res)
+          handleClose()
       })
     }
 
@@ -48,7 +56,19 @@ export default function Confirm(props) {
       return (
         <Dialog onClose={handleClose} open={open}>     
             <DialogTitle>Confirm Delete</DialogTitle>
-        
+                <Card sx={{ minWidth: 275 }}>
+                    <CardContent>
+                        <Typography sx={{ fontSize: 18 }} color="text.secondary" gutterBottom>
+                        {rowData.last_name}, {rowData. first_name}
+                        </Typography>
+                        <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                        {rowData.mrn}
+                        </Typography>
+                    </CardContent>
+                    <CardActions>
+                        <Button onClick= {() => deletePatient(rowData.id)}>Delete</Button>
+                        <Button onClick= {() => handleClose()}>Cancel</Button>                    </CardActions>
+                    </Card>
         </Dialog>
       )
   }
